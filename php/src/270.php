da88da614cff55228b73f5b4041a63b80f02afe9
Arantxa Gandia <118548804+Arantxaa31@users.noreply.github.com>
2023-10-02 19:13:57 +0200

    [ADD] actividades 270-272

---
+++ php/src/270.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ej 270</title>
+</head>
+<body>
+<?php
+include('270a.php')
+?>
+</body>
+</html>
--- php/src/functions.php
+++ php/src/functions.php
@@ -77,3 +77,87 @@ function peseta2euros(float $quantitat, $conticacio = 166){
 function euro2pesetes(float $quantitat, $conticacio = 166){
     return round($quantitat * $conticacio);
 }
+
+function desplacament(string  $cadena, int $des){
+    if ($des < 0){
+        return 'El desplaçament no pot ser negatiu';
+    }
+    $abecedario = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+    $resultado = '';
+    for ($i = 0; $i < strlen($cadena); $i++) {
+        $caracter = strtoupper($cadena[$i]);
+        if(ctype_alpha($caracter)){
+            $indice = strpos($abecedario,$caracter);
+            $indiceNuevo = ($indice + $des) % 26;
+            $nuevaLetra = $abecedario[$indiceNuevo];
+            $resultado .= $nuevaLetra;
+        }else{
+            $resultado .= $caracter;
+        }
+    }
+    return  $resultado;
+}
+function fecha_inglesa($date){
+    $fechaPartida = explode('.',$date);
+    return $fechaPartida[2] . '/' . $fechaPartida[1] . '/' . $fechaPartida[0];
+}
+
+function vell($array){
+    $fechaMasVieja = null;
+    $llaveFechaMasVieja = null;
+
+    foreach ($array as $llave => $fecha){
+        $fechaIngles = fecha_inglesa($fecha);
+        $fechaConvertida = strtotime($fechaIngles);
+        if($fechaConvertida < $fechaMasVieja || $fechaMasVieja === null){
+            $fechaMasVieja = $fechaConvertida;
+            $llaveFechaMasVieja = $llave;
+        }
+    }
+    return $llaveFechaMasVieja;
+}
+
+function laureado($array){
+    $vecesRepetidas = array_count_values($array);
+    arsort($vecesRepetidas);
+    $masRepetido = reset($vecesRepetidas);
+    $valorMasRepetido = array_keys($vecesRepetidas,$masRepetido);
+    return$valorMasRepetido[0];
+}
+
+function any($fecha){
+    $fechaPartida = explode('.',$fecha);
+    return $fechaPartida[2];
+}
+
+function jove($nacimientos, $fechas){
+    $atletaMasJoven = null;
+    $menorDiferencia = null;
+    $atletasMasJovenArray = [];
+    foreach ($nacimientos as $indice => $natalici){
+        $fecha = $fechas[$indice];
+        $anyoRecord = any($fecha);
+        $diferencia = $anyoRecord - intval($natalici);
+        if ($diferencia < $menorDiferencia || $menorDiferencia === null){
+            $menorDiferencia = $diferencia;
+            $atletaMasJoven = $indice;
+        }
+    }
+    $atletasMasJovenArray = [$atletaMasJoven,$menorDiferencia];
+    return $atletasMasJovenArray;
+
+}
+function array_column_ext($array, $columnkey, $indexkey = null) {
+    $result = array();
+    foreach ($array as $subarray => $value) {
+        if (array_key_exists($columnkey,$value)) { $val = $array[$subarray][$columnkey]; }
+        else if ($columnkey === null) { $val = $value; }
+        else { continue; }
+
+        if ($indexkey === null) { $result[] = $val; }
+        elseif ($indexkey == -1 || array_key_exists($indexkey,$value)) {
+            $result[($indexkey == -1)?$subarray:$array[$subarray][$indexkey]] = $val;
+        }
+    }
+    return $result;
+}
